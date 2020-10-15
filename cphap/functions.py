97c09aeb909a3bbc50d51604ea90e8945e733965
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-15 16:22:52 +0900

    find_t が論文の内容を満たしていない問題を修正

--- cphap/functions.py
+++ cphap/functions.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 
 import torch
 import numpy as np
@@ -12,7 +12,7 @@ __all__ = ["find_t", "plot_kde", "han"]
 
 def find_t(
     feature_map: torch.Tensor, margin: float = 0.05, return_raw: bool = False
-) -> Union[Tuple[np.ndarray, float, gaussian_kde], float]:
+) -> torch.Tensor:
     """
 
     Args:
@@ -38,37 +38,47 @@ def find_t(
 
     """
     with torch.no_grad():
+        feature_map = torch.flatten(feature_map)
+        values, indices = feature_map.sort()
+        threshold_idx: int = int(len(values) * (1.0 - margin))
+        threshold = values[threshold_idx]
+
+    return threshold
+
+
+def plot_kde(feature_map: torch.Tensor, plot: bool = True, margin: float = 0.05) -> None:
+    tmp = feature_map
+    with torch.no_grad():
+        feature_map = torch.flatten(feature_map)
         min_value: int = feature_map.min().item()
         max_value: int = feature_map.max().item()
-        seq_len: int = feature_map.shape[1]
 
         kernel = gaussian_kde(feature_map)  # カーネル密度推定
-        xs = np.linspace(min_value, max_value, seq_len ** 2)  # 全区間積分の範囲
+        xs = np.linspace(min_value, max_value, 5000)  # 全区間積分の範囲
         ys = kernel(xs)
 
-        cdf = cumtrapz(ys, xs)  # 積分して累積分布関数を計算
-        idx = np.searchsorted(cdf, 1 - margin)
-        print(xs[idx])
-
-    if return_raw:
-        return xs, idx.item(), kernel
-    else:
-        return xs[idx].item()  # threshold
-
-
-def plot_kde(feature_map: torch.Tensor, plot: bool = True) -> None:
-    xs, idx, kernel = find_t(feature_map, return_raw=True)
-    ys = kernel(xs)
-    min_value: int = feature_map.min().item()
-    max_value: int = feature_map.max().item()
+    threshold = find_t(tmp).numpy()
     plt.plot(xs, kernel(xs))
-    plt.fill_between(xs[:idx], 0, ys[:idx])
+
+    plt.fill_between(xs[xs > threshold], 0, ys[xs > threshold])
     plt.xlim(min_value, max_value)
     if plot:
         plt.show()
 
 
 def han(feature_map: torch.Tensor, margin: float = 0.05):
+    # feature_map.shape == [N, L]
     threshold = find_t(feature_map, margin)
-    _, han_values = np.where(feature_map > threshold)
-    return han_values
+    if threshold:
+        all_values = []
+        for sample in range(feature_map.shape[0]):
+            values = {i for i in range(feature_map.shape[-1]) if feature_map[sample, i] > threshold}
+            all_values.append(values)
+    # _, han_values = np.where(feature_map > threshold)
+        return all_values
+    else:
+        raise Exception
+
+
+def hap(han_result, conv_in):
+    return conv_in[han_result]
